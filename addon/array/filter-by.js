055219a5d53d2f86ee04eac8c931a60f9b995684
Kelly Selden <kellyselden@gmail.com>
2017-07-15 20:43:40 -0700

    allow optional args

--- addon/array/filter-by.js
+++ addon/array/filter-by.js
@@ -7,11 +7,15 @@ const defaultValue = [];
 export default createClassComputed(
   [false, true, false],
   (array, key, value) => {
-    return computed(normalizeArrayKey(array, [key]), value, (array, value) => {
+    let args = [normalizeArrayKey(array, [key])];
+    if (value) {
+      args.push(value);
+    }
+    return computed(...args, (array, ...args) => {
       if (!array || !key) {
         return defaultValue;
       }
-      return array.filterBy(key, value);
+      return array.filterBy(key, ...args);
     });
   }
 );
